48dbcf90c1d63799b10df07d773c6c477cf1b530
DerLed <lediburit@mail.ru>
2021-12-08 21:53:50 +0300

    2.4

--- bboard/views.py
+++ bboard/views.py
@@ -3,12 +3,21 @@
 # from django.template import loader
 from django.shortcuts import render
 from .models import Bb
+from .models import Rubric
 
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    contex = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render (request, 'bboard/by_rubric.html', contex)
 
 def index(request):
     # template = loader.get_template('bboard/index.html')
     bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    contex = {'bbs':bbs, 'rubrics':rubrics}
     # context = {'bbs': bbs}
 
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+    return render(request, 'bboard/index.html', contex)
 
